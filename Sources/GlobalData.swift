9a1c9874e32367b129b4f823eb54b158fe4c0785
david <daixeibing@126.com>
2017-06-17 23:07:47 +0800

    df

--- Sources/GlobalData.swift
+++ Sources/GlobalData.swift
@@ -23,12 +23,12 @@ extension GlobalData {
     // 是否需要从新请求 access_token
     func availableAccessToken () -> Bool {
         print("accessTokenDic:\(accessTokenDic)")
-        if let _ = accessTokenDic,
-            let _ = accessTokenDic?["access_token"] as? String,
-            let expires_in = accessTokenDic?["expires_in"] as? TimeInterval,
-            let saveDate = accessTokenDic?["saveDate"] as? Date{
+        if let token = accessTokenDic,
+            let _ = token["access_token"] as? String,
+            let expires_in = token["expires_in"] as? TimeInterval,
+            let saveDate = token["saveDate"] as? TimeInterval{
             
-            if Date().timeIntervalSince1970 - saveDate.timeIntervalSince1970 < TimeInterval(expires_in - 600) {
+            if Date().timeIntervalSince1970 - saveDate < TimeInterval(expires_in - 600) {
                 return true
             }
             return false
