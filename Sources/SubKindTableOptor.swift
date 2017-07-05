9356d9e8129c97c7b44453964f7ba90c78b2d21b
david <daixeibing@126.com>
2017-07-05 19:41:45 +0800

    -100 3

--- Sources/SubKindTableOptor.swift
+++ Sources/SubKindTableOptor.swift
@@ -119,10 +119,13 @@ class SubKindTableOptor: DBBaseOperator {
             
             if let m_foods = FoodTableOptor.shared.queryFoods(pid: m_subkind.id, ppid: pid) {
                 m_subkind.foods = m_foods
+                if m_foods.count == 0 {
+                    if let m_foods = FoodTableOptor.shared.queryFoods(pid: -1, ppid: pid) {
+                        m_subkind.foods = m_foods
+                    }
+                }
             }
-            if let m_foods = FoodTableOptor.shared.queryFoods(pid: -1, ppid: pid) {
-                m_subkind.foods = m_foods
-            }
+            
             
 
             m_subkindList.append(m_subkind)
